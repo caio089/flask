cab2ab339e9746cf7df4842c4e304f3b7655d27c
caiocampos <ccamposs2007@gmail.com>
2025-06-09 09:58:03 -0300

    etrapa final com template

--- app.py
+++ app.py
@@ -3,30 +3,23 @@ from gemini import content
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def home(): 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    resposta = None
 
-    return render_template('index.html')
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            pergunta = data.get('pergunta')
+        else:
+            pergunta = request.form.get('pergunta')
 
-    # Tenta pegar do JSON
-    if request.is_json:
-        data = request.get_json()
-        pergunta = data.get('pergunta')
-    else:
-        # Tenta pegar do formulário (form-data ou x-www-form-urlencoded)
-        pergunta = request.form.get('pergunta')
+        if pergunta:
+            resposta = content(pergunta)
 
-    if pergunta:
-        resposta = content(pergunta)
-        return resposta, 200
-    else:
-        return 'Nenhuma pergunta válida.', 400
-
-
-@app.route('/', methods=['GET'])
-def status():
-    return 'API funcionando. Use POST para enviar sua pergunta.', 200
+    return render_template('index.html', resposta=resposta)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
