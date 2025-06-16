b72320c7af25405b67818d67d09756b80f30616d
caiocampos <ccamposs2007@gmail.com>
2025-06-16 09:03:25 -0300

    db

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, request, render_template
 from gemini import content
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///historico.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Pesquisa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    texto = db.Column(db.String(200), nullable=False)
+    data_hora = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -15,11 +29,21 @@ def home():
             pergunta = request.form.get('pergunta')
 
         if pergunta:
+            # 🆕 SALVAR NO BANCO
+            nova_pesquisa = Pesquisa(texto=pergunta)
+            db.session.add(nova_pesquisa)
+            db.session.commit()
+
             resposta = content(pergunta)
 
-    return render_template('index.html', resposta=resposta)
+    # 🆕 PEGAR O HISTÓRICO PARA MOSTRAR NO HTML
+    historico = Pesquisa.query.order_by(Pesquisa.data_hora.desc()).all()
+
+    return render_template('index.html', resposta=resposta, historico=historico)
 
 
 if __name__ == '__main__':
+ with app.app_context():
+    db.create_all()
     app.run(debug=True)
 
